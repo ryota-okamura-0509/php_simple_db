cf881df0ce0b56d3a959300850c74892acfb52fd
ryota-okamura-0509 <r_okamura@codmon.co.jp>
2025-02-08 12:00:43 +0900

    InputBufferクラスのことを完全に忘れていたので追加

--- app/index.php
+++ app/index.php
@@ -1,25 +1,32 @@
 <?php
 
+class InputBuffer {
+    public $buffer;
+
+    public function __construct() {
+        $this->buffer = "";
+    }
+}
+
 function print_prompt() {
     echo "db > ";
 }
 
-function read_input() {
+function read_input(InputBuffer $inputBuffer) {
     $input = trim(fgets(STDIN));
     if ($input === false) {
         echo "Error reading input\n";
         exit(1);
     }
-    return $input;
+    
+    // 改行を削除して格納
+    $inputBuffer->buffer = trim($input);
 }
 
+$input_buffer = new InputBuffer();
 while (true) {
     print_prompt();
-    $input = read_input();
+    $input = read_input($input_buffer);
 
-    if ($input === ".exit") {
-        exit(0);
-    } else {
-        echo "Unrecognized command '$input'.\n";
-    }
+    
 }
